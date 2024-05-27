3aaceb48283e93701fa95b18414fb3f59f06c30c
JoséMiguel Marte <tumusicalatina@gmail.com>
2024-05-27 16:50:48 -0400

    click event

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -21,12 +21,19 @@ const ListGroup = () => {
             name: "Chicago",
         },
     ];
+    // items = [];
+    
+    const getMessage = () => {
+        items.length === 0? <p>No item found</p> : "Items";
+
+    }
     return (
         <>
             <h1>List</h1>
+            {getMessage()}
         <ul className="list-group">
        {items.map((item) => (
-                <li className="list-group-item" key={item.id}>
+                <li className="list-group-item" key={item.id} onClick={() => console.log(item.name)}>
                     {item.name}
                 </li>
             ))}
