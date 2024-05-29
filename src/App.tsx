c773f76f8bff0fffd04b173d3c02b6dc06bb90aa
JoséMiguel Marte <tumusicalatina@gmail.com>
2024-05-29 16:53:10 -0400

    finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,39 @@
-import ListGroup from "./components/ListGroup"; 
+import { useState } from "react";
+import Alert from "./components/Alert";
+import Button from "./components/Button";
+import ListGroup from "./components/ListGroup";
 
 const App = () => {
+  const [alertVisible, setAlertVisible] = useState(false);
+  let items = [
+    "New York",
 
-    let items = [
-       
-           
-             "New York",
-       
-           
-            "Rhode Island",
-       
-           
-            "San Francisco",
-       
-           
-             "Philadelphia",
-       
-            
-            "Chicago",
-       
-    ];
-    const handleSelectItem = (item: string) => {
-        console.log(item);
-    }
-        return (<div><ListGroup items={items} heading="List of cities" onSelectitem={handleSelectItem} /></div>);
-}
+    "Rhode Island",
+
+    "San Francisco",
+
+    "Philadelphia",
+
+    "Chicago",
+  ];
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+  return (
+    <div>
+      <ListGroup
+        items={items}
+        heading="List of cities"
+        onSelectItem={handleSelectItem}
+      />
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisible(false)}>
+          Hola <span> Mundo@</span>
+        </Alert>
+      )}
+      <Button onClick={() => setAlertVisible(true)}>Boton</Button>
+    </div>
+  );
+};
 
 export default App;
